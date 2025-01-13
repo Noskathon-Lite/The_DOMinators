02e61d20f705e1f9d1223ba947f777464cd442bd
Madhu kunwar <kcmadhu081@gmail.com>
2025-01-13 09:47:18 +0545

    dialog

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from './components/logincomponents/theme-provider';
 import MainLayout from './layouts/MainLayout';
+import { Dialog } from './components/ui/dialog';
 import { Toaster } from './components/ui/toaster';
 
 
@@ -10,6 +11,7 @@ function App() {
     <ThemeProvider defaultTheme="system" storageKey="climate-grow-theme">
       <Router>
         <MainLayout />
+        <Dialog />
         <Toaster/>
       </Router>
     </ThemeProvider>

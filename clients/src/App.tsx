3963f2a2f073194973be248e06f049a05768fdec
Madhu kunwar <kcmadhu081@gmail.com>
2025-01-13 10:14:41 +0545

    chatbot

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -1,17 +1,15 @@
-import { Navigate, BrowserRouter as Router } from 'react-router-dom';
+import {  BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from './components/logincomponents/theme-provider';
 import MainLayout from './layouts/MainLayout';
-import { Dialog } from './components/ui/dialog';
 import { Toaster } from './components/ui/toaster';
-
-
+import { Chatbot } from './components/common/Chatbot';
 function App() {
 
   return (
     <ThemeProvider defaultTheme="system" storageKey="climate-grow-theme">
       <Router>
         <MainLayout />
-        <Dialog />
+        <Chatbot />
         <Toaster/>
       </Router>
     </ThemeProvider>
